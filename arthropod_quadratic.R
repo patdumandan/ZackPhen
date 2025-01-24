7d8fe66d8ddf4c8959b5f0bb31115b126275375f
Patricia Kaye Tahura Dumandan <patricia.dumandan@slu.se>
2025-01-24 15:36:04 +0100

    change model structure for arthropods

--- arthropod_quadratic.R
+++ arthropod_quadratic.R
@@ -82,8 +82,8 @@ p1=ggplot(pho_dat, aes(x=DOY, y=log(TotalCatch1)))+geom_point()+facet_wrap(~Plot
 s1=ggplot(sci_dat, aes(x=DOY, y=log(TotalCatch1)))+geom_point()+facet_wrap(~Plot.ID)+
   geom_smooth(method="gam")+ggtitle("Sciaridae")
 
-ly1=ggplot(lyc_dat, aes(x=DOY, y=log(TotalCatch1)))+geom_point()+facet_wrap(~Plot.ID)+
-    geom_smooth(method="gam")+ggtitle("Lycosidae")
+ly1=ggplot(lyc_dat, aes(x=DOY, y=log(TotalCatch1), col=as.factor(Year)))+geom_point()+facet_wrap(~Plot.ID)+
+    ggtitle("Lycosidae")+scale_colour_viridis_d()
 
 ggarrange(b1,c1,i1,a1,ly1, s1, p1, n1, m1, l1, cu1, co1, ch1)
 
@@ -91,172 +91,173 @@ ggarrange(b1,c1,i1,a1,ly1, s1, p1, n1, m1, l1, cu1, co1, ch1)
 
 #Collembola
 
-col_mod=glm(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
+col_mod=glmer(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
                  DOYs*years + # timing and long-term trend interaction
-                 poly(DOYs, 2)* years, #curve and long-term trend interaction
+                 poly(DOYs, 2)* years+ #curve and long-term trend interaction
+              (years|Plot.ID), #random effects to account for plot-level differences in trends
                family = "poisson", data=col_dat)
 
 col_preds=as.vector(predict(col_mod, type="response"))%>%cbind(col_dat)
 colnames(col_preds)[1]="preds"
 
 col_plot1=ggplot(col_preds, aes(x=DOY, y=preds, col=as.factor(Year)))+geom_line()+
-  theme_classic()+ggtitle("Collembola")+scale_color_viridis_d()
-#  geom_point(aes(x=DOY, y=TotalCatch1), col="black")
+  theme_classic()+ggtitle("Collembola")+scale_color_viridis_d()+
+  facet_wrap(~Plot.ID)
 
 #Acari
-aca_mod=glm(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
+aca_mod=glmer(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
               DOYs*years + # timing and long-term trend interaction
-              poly(DOYs, 2)* years, #curve and long-term trend interaction
+              poly(DOYs, 2)* years+(years|Plot.ID), #curve and long-term trend interaction
             family = "poisson", data=aca_dat)
 
 aca_preds=as.vector(predict(aca_mod, type="response"))%>%cbind(aca_dat)
 colnames(aca_preds)[1]="preds"
 
 aca_plot1=ggplot(aca_preds, aes(x=DOY, y=preds, col=as.factor(Year)))+geom_line()+
-  theme_classic()+ggtitle("Acari")+scale_color_viridis_d()
+  theme_classic()+ggtitle("Acari")+scale_color_viridis_d()+facet_wrap(~Plot.ID)
 #  geom_point(aes(x=DOY, y=TotalCatch1), col="black")
 
 #Ichneumonidae
-ich_mod=glm(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
+ich_mod=glmer(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
               DOYs*years + # timing and long-term trend interaction
-              poly(DOYs, 2)* years, #curve and long-term trend interaction
+              poly(DOYs, 2)* years+(years|Plot.ID), #curve and long-term trend interaction
             family = "poisson", data=ich_dat)
 
 ich_preds=as.vector(predict(ich_mod, type="response"))%>%cbind(ich_dat)
 colnames(ich_preds)[1]="preds"
 
 ich_plot1=ggplot(ich_preds, aes(x=DOY, y=preds, col=as.factor(Year)))+geom_line()+
-  theme_classic()+ggtitle("Ichneumonidae")+scale_color_viridis_d()
+  theme_classic()+ggtitle("Ichneumonidae")+scale_color_viridis_d()+facet_wrap(~Plot.ID)
 #  geom_point(aes(x=DOY, y=TotalCatch1), col="black")
 
 #Bombus
-bom_mod=glm(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
+bom_mod=glmer(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
               DOYs*years + # timing and long-term trend interaction
-              poly(DOYs, 2)* years, #curve and long-term trend interaction
+              poly(DOYs, 2)* years+(years|Plot.ID), #curve and long-term trend interaction
             family = "poisson", data=bom_dat)
 
 bom_preds=as.vector(predict(bom_mod, type="response"))%>%cbind(bom_dat)
 colnames(bom_preds)[1]="preds"
 
 bom_plot1=ggplot(bom_preds, aes(x=DOY, y=preds, col=as.factor(Year)))+geom_line()+
-  theme_classic()+ggtitle("Bombus")+scale_color_viridis_d()
+  theme_classic()+ggtitle("Bombus")+scale_color_viridis_d()+facet_wrap(~Plot.ID)
 #  geom_point(aes(x=DOY, y=TotalCatch1), col="black")
 
 #Lycosidae
 lyc_mod=glm(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
               DOYs*years + # timing and long-term trend interaction
-              poly(DOYs, 2)* years, #curve and long-term trend interaction
+              poly(DOYs, 2)* years+(years|Plot.ID), #curve and long-term trend interaction
             family = "poisson", data=lyc_dat)
 
 lyc_preds=as.vector(predict(lyc_mod, type="response"))%>%cbind(lyc_dat)
 colnames(lyc_preds)[1]="preds"
 
 lyc_plot1=ggplot(lyc_preds, aes(x=DOY, y=preds, col=as.factor(Year)))+geom_line()+
-  theme_classic()+ggtitle("Lycosidae")+scale_color_viridis_d()
+  theme_classic()+ggtitle("Lycosidae")+scale_color_viridis_d()+facet_wrap(~Plot.ID)
 #  geom_point(aes(x=DOY, y=TotalCatch1), col="black")
 
 #Sciaridae
-sci_mod=glm(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
+sci_mod=glmer(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
               DOYs*years + # timing and long-term trend interaction
-              poly(DOYs, 2)* years, #curve and long-term trend interaction
+              poly(DOYs, 2)* years+(years|Plot.ID), #curve and long-term trend interaction
             family = "poisson", data=sci_dat)
 
 sci_preds=as.vector(predict(sci_mod, type="response"))%>%cbind(sci_dat)
 colnames(sci_preds)[1]="preds"
 
 sci_plot1=ggplot(sci_preds, aes(x=DOY, y=preds, col=as.factor(Year)))+geom_line()+
-  theme_classic()+ggtitle("Sciaridae")+scale_color_viridis_d()
+  theme_classic()+ggtitle("Sciaridae")+scale_color_viridis_d()+facet_wrap(~Plot.ID)
 #  geom_point(aes(x=DOY, y=TotalCatch1), col="black")
 
 #Phoridae
-pho_mod=glm(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
+pho_mod=glmer(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
               DOYs*years + # timing and long-term trend interaction
-              poly(DOYs, 2)* years, #curve and long-term trend interaction
+              poly(DOYs, 2)* years+(years|Plot.ID), #curve and long-term trend interaction
             family = "poisson", data=pho_dat)
 
 pho_preds=as.vector(predict(pho_mod, type="response"))%>%cbind(pho_dat)
 colnames(pho_preds)[1]="preds"
 
 pho_plot1=ggplot(pho_preds, aes(x=DOY, y=preds, col=as.factor(Year)))+geom_line()+
-  theme_classic()+ggtitle("Phoridae")+scale_color_viridis_d()
+  theme_classic()+ggtitle("Phoridae")+scale_color_viridis_d()+facet_wrap(~Plot.ID)
 #  geom_point(aes(x=DOY, y=TotalCatch1), col="black")
 
 #Nymphalidae
-nym_mod=glm(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
+nym_mod=glmer(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
               DOYs*years + # timing and long-term trend interaction
-              poly(DOYs, 2)* years, #curve and long-term trend interaction
+              poly(DOYs, 2)* years+(years|Plot.ID), #curve and long-term trend interaction
             family = "poisson", data=nym_dat)
 
 nym_preds=as.vector(predict(nym_mod, type="response"))%>%cbind(nym_dat)
 colnames(nym_preds)[1]="preds"
 
 nym_plot1=ggplot(nym_preds, aes(x=DOY, y=preds, col=as.factor(Year)))+geom_line()+
-  theme_classic()+ggtitle("Nymphalidae")+scale_color_viridis_d()
+  theme_classic()+ggtitle("Nymphalidae")+scale_color_viridis_d()+facet_wrap(~Plot.ID)
 #  geom_point(aes(x=DOY, y=TotalCatch1), col="black")
 
 #Muscidae
-mus_mod=glm(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
+mus_mod=glmer(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
               DOYs*years + # timing and long-term trend interaction
-              poly(DOYs, 2)* years, #curve and long-term trend interaction
+              poly(DOYs, 2)* years+(years|Plot.ID), #curve and long-term trend interaction
             family = "poisson", data=mus_dat)
 
 mus_preds=as.vector(predict(mus_mod, type="response"))%>%cbind(mus_dat)
 colnames(mus_preds)[1]="preds"
 
 mus_plot1=ggplot(mus_preds, aes(x=DOY, y=preds, col=as.factor(Year)))+geom_line()+
-  theme_classic()+ggtitle("Muscidae")+scale_color_viridis_d()
+  theme_classic()+ggtitle("Muscidae")+scale_color_viridis_d()+facet_wrap(~Plot.ID)
 #  geom_point(aes(x=DOY, y=TotalCatch1), col="black")
 
 #Linyphiidae
-lin_mod=glm(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
+lin_mod=glmer(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
               DOYs*years + # timing and long-term trend interaction
-              poly(DOYs, 2)* years, #curve and long-term trend interaction
+              poly(DOYs, 2)* years+(years|Plot.ID), #curve and long-term trend interaction
             family = "poisson", data=lin_dat)
 
 lin_preds=as.vector(predict(lin_mod, type="response"))%>%cbind(lin_dat)
 colnames(lin_preds)[1]="preds"
 
 lin_plot1=ggplot(lin_preds, aes(x=DOY, y=preds, col=as.factor(Year)))+geom_line()+
-  theme_classic()+ggtitle("Linyphiidae")+scale_color_viridis_d()
-#  geom_point(aes(x=DOY, y=TotalCatch1), col="black")
+  theme_classic()+ggtitle("Linyphiidae")+scale_color_viridis_d()+facet_wrap(~Plot.ID)
+
 
 #Culicidae
-cul_mod=glm(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
+cul_mod=glmer(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
               DOYs*years + # timing and long-term trend interaction
-              poly(DOYs, 2)* years, #curve and long-term trend interaction
+              poly(DOYs, 2)* years+(years|Plot.ID), #curve and long-term trend interaction
             family = "poisson", data=cul_dat)
 
 cul_preds=as.vector(predict(cul_mod, type="response"))%>%cbind(cul_dat)
 colnames(cul_preds)[1]="preds"
 
 cul_plot1=ggplot(cul_preds, aes(x=DOY, y=preds, col=as.factor(Year)))+geom_line()+
-  theme_classic()+ggtitle("Culicidae")+scale_color_viridis_d()
+  theme_classic()+ggtitle("Culicidae")+scale_color_viridis_d()+facet_wrap(~Plot.ID)
 #  geom_point(aes(x=DOY, y=TotalCatch1), col="black")
 
 #Chironomidae
-chi_mod=glm(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
+chi_mod=glmer(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
               DOYs*years + # timing and long-term trend interaction
-              poly(DOYs, 2)* years, #curve and long-term trend interaction
+              poly(DOYs, 2)* years+(years|Plot.ID), #curve and long-term trend interaction
             family = "poisson", data=chi_dat)
 
 chi_preds=as.vector(predict(chi_mod, type="response"))%>%cbind(chi_dat)
 colnames(chi_preds)[1]="preds"
 
 chi_plot1=ggplot(chi_preds, aes(x=DOY, y=preds, col=as.factor(Year)))+geom_line()+
-  theme_classic()+ggtitle("Chironomidae")+scale_color_viridis_d()
+  theme_classic()+ggtitle("Chironomidae")+scale_color_viridis_d()+facet_wrap(~Plot.ID)
 #  geom_point(aes(x=DOY, y=TotalCatch1), col="black")
 
 #Coccoidea
-coc_mod=glm(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
+coc_mod=glmer(TotalCatch1~ DOYs+ poly(DOYs,2) + years + # base linear terms
               DOYs*years + # timing and long-term trend interaction
-              poly(DOYs, 2)* years, #curve and long-term trend interaction
+              poly(DOYs, 2)* years+(years|Plot.ID), #curve and long-term trend interaction
             family = "poisson", data=coc_dat)
 
 coc_preds=as.vector(predict(coc_mod, type="response"))%>%cbind(coc_dat)
 colnames(coc_preds)[1]="preds"
 
 coc_plot1=ggplot(coc_preds, aes(x=DOY, y=preds, col=as.factor(Year)))+geom_line()+
-  theme_classic()+ggtitle("Coccoidea")+scale_color_viridis_d()
+  theme_classic()+ggtitle("Coccoidea")+scale_color_viridis_d()+facet_wrap(~Plot.ID)
 #  geom_point(aes(x=DOY, y=TotalCatch1), col="black")
 
 ggarrange(bom_plot1, col_plot1, ich_plot1, aca_plot1,
