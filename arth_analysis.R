40ebde233047cb2a3c24564ea3c881cd0d10b07e
Patricia <patdumandan@gmail.com>
2025-05-15 04:06:07 +0200

    add other spp code and toy model code

--- arth_analysis.R
+++ arth_analysis.R
@@ -28,7 +28,7 @@ arth_plots=arth_dat%>%filter(!Plot.ID%in%c("Art1","Art4", "Art6"))%>%group_by(Ho
 
 arth_data=arth_dat%>%filter(!Plot.ID%in%c("Art1","Art4", "Art6"), !Year<1996)%>%
   select(-CatchID, -UnitID, -X)%>%
-  mutate(DOYsq=DOY^2)
+  mutate(DOYsq=I(DOY^2))
 
 #rescale data
 
@@ -47,7 +47,7 @@ chi_dat=arth_data%>%filter(HoyeTaxon=="Chironomidae")
 coc_dat=arth_data%>%filter(HoyeTaxon=="Coccoidea")
 cul_dat=arth_data%>%filter(HoyeTaxon=="Culicidae")
 lin_dat=arth_data%>%filter(HoyeTaxon=="Linyphiidae")
-mus_dat=arth_data%>%filter(HoyeTaxon=="Muscidae")
+mus_dat=arth_data%>%filter(HoyeTaxon=="Muscidae") #house flies
 nym_dat=arth_data%>%filter(HoyeTaxon=="Nymphalidae")
 pho_dat=arth_data%>%filter(HoyeTaxon=="Phoridae")
 sci_dat=arth_data%>%filter(HoyeTaxon=="Sciaridae")
@@ -63,7 +63,7 @@ col_mod_nb=glmer.nb(TotalCatch1 ~  DOYs+ DOYsqs + years +
                 (1|Plot.ID),
                 family="nbinom",
                 data=col_dat,
-                control = lmerControl(optimizer = "bobyqa"))
+                control = glmerControl(optimizer = "bobyqa"))
 
 col_mod_pois=glmer(TotalCatch1 ~  DOYs+ DOYsqs + years +
                      DOYs * years+
@@ -250,7 +250,92 @@ peak_std = -bt / (2 * ct)
 peak_doy = peak_std * sd(mus_dat$DOY) + mean(mus_dat$DOY)
 par(mfrow=c(1,2))
 plot(peak_doy ~ yrs, type="l", ylab="Peak DOY", xlab="Year", main="Mus. Estimated Phenological Peak")
-
 plot(ct~ yrs, type="l", ylab="c", xlab="Year", main="Mus. Phenological Shape")
 
+#Bombus
+bom_mod_nb=glmer.nb(TotalCatch1 ~  DOYs+ DOYsqs + years +
+                      DOYs * years+
+                      DOYsqs * years+
+                      (1|Plot.ID),
+                    family="nbinom",
+                    data=bom_dat,
+                    control = glmerControl(optimizer = "bobyqa"))
 
+bom_mod_pois=glmer(TotalCatch1 ~  DOYs+ DOYsqs + years +
+                     DOYs * years+
+                     DOYsqs * years+
+                     (1|Plot.ID),#random effects to account for plot-level differences in trends
+                   family="poisson", data=bom_dat)
+
+AIC(bom_mod_nb)
+AIC(bom_mod_pois)
+
+bom_preds=as.vector(predict(bom_mod_nb, type="response"))%>%cbind(bom_dat)
+colnames(bom_preds)[1]="preds"
+
+bom_preds1=bom_preds%>%
+  rename(predicted=preds, actual=TotalCatch1)%>%
+  pivot_longer(cols=c("predicted", "actual"), names_to="value_type")
+
+ggplot(bom_preds, aes(x=DOY, y=preds, col=as.factor(Year)))+geom_line()+
+  theme_classic()+ggtitle("Bombus")+scale_color_viridis_d()+
+  geom_point(aes(x=DOY, y=TotalCatch1, col=as.factor(Year)), size=1)+
+  facet_wrap(~Plot.ID)
+
+ModelMetrics::rmse(bom_preds$preds, bom_preds$TotalCatch1)
+
+#determine change in peak dates using derivatives
+
+#given y=ax^2+bx+c, we can assume that change in (a) over years can
+#tell us if the curve is widening or narrowing,
+#and the point where the 2nd derivative (2ax+b)=0 is the peak
+
+#to get the 2nd derivative wrt to DOY, we assume that
+#ax^2=DOY^2(B1)+DOY^2:year(B4)*year, bx=DOY(B2)+DOY:year(B5)*year
+
+yrs=1996:2024
+yrs_std = (yrs - mean(bom_dat$year)) / sd(bom_dat$year)
+
+coefs=fixef(bom_mod_nb)
+
+b0 <- coefs['(Intercept)']
+b1 <- coefs['DOYs']
+b2 <- coefs['DOYsqs']
+b3 <- coefs['years']
+b4 <- coefs['DOYs:years']
+b5 <- coefs['DOYsqs:years']
+
+bt=b1+b4*yrs
+ct= b2+b5*yrs
+peak_std = -bt / (2 * ct)
+
+# Convert back from standardized DOY to actual DOY
+
+peak_doy = peak_std * sd(bom_dat$DOY) + mean(bom_dat$DOY)
+peak_dat=cbind(peak_doy, yrs)
+
+par(mfrow=c(1,2))
+peak_doy = peak_std * sd(mus_dat$DOY) + mean(mus_dat$DOY)
+par(mfrow=c(1,2))
+plot(peak_doy ~ yrs, type="l", ylab="Peak DOY", xlab="Year", main="Bombus Estimated Phenological Peak")
+plot(ct~ yrs, type="l", ylab="c", xlab="Year", main="Bombus Phenological Shape")
+
+
+
+ggplot(peak_dat, aes(x = yrs, y = peak_doy)) +
+  geom_line() +
+  geom_point() +
+  labs(title = "Bombus Estimated Phenological Peak",
+       x = "Year",
+       y = "Peak DOY") +
+  theme_classic()
+
+c_dat=cbind(ct, yrs)
+
+ggplot(c_dat, aes(x = yrs, y = ct)) +
+  geom_line() +
+  geom_point() +
+  labs(title = "Bombus Phenological Shape",
+       x = "Year",
+       y = "c") +
+  theme_classic()
