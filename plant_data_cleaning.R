e7ae38ff51d3bcccf405b053a73192872d1d1898
Patricia <patdumandan@gmail.com>
2025-09-05 14:54:17 +0200

    add data checks

--- plant_data_cleaning.R
+++ plant_data_cleaning.R
@@ -15,7 +15,7 @@ cas_dat$Date=as.POSIXct(cas_dat$Date, tz="GMT", format = "%Y-%m-%d")
 cas_dat_raw=cas_dat%>%
   mutate(year=year(Date), month=month(Date), dia=day(Date), species="Cassiope")%>%
   mutate(DOY=yday(Date))%>%
-  select(-Field_remarks, -General_remarks, -Date)%>%
+  select(-Field_remarks, -General_remarks)%>%
   filter(Plot%in%c("Cas1", "Cas2", "Cas3", "Cas4"))
 
 ###dryas####
@@ -27,7 +27,7 @@ dry_dat$Date=as.POSIXct(dry_dat$Date, tz="GMT", format = "%Y-%m-%d")
 dry_dat_raw=dry_dat%>%
   mutate(year=year(Date), month=month(Date), dia=day(Date), species="Dryas")%>%
   mutate(DOY=yday(Date))%>%
-  select(-Field_remarks, -General_remarks, -Date)%>%
+  select(-Field_remarks, -General_remarks)%>%
   filter(Plot%in%c("Dry1", "Dry2", "Dry3", "Dry4", "Dry5", "Dry6"))
 
 ###papaver####
@@ -51,7 +51,7 @@ sal_dat$Date=as.POSIXct(sal_dat$Date, tz="GMT", format = "%Y-%m-%d")
 sal_dat_raw=sal_dat%>%
   mutate(year=year(Date), month=month(Date), dia=day(Date), species="Salix")%>%
   mutate(DOY=yday(Date))%>%
-  select(-Field_remarks, -General_remarks, -Date)%>%
+  select(-Field_remarks, -General_remarks)%>%
   filter(Plot%in%c("Sal1", "Sal2", "Sal3", "Sal4", "Sal5", "Sal6", "Sal7"))
 
 ###saxifraga####
@@ -63,7 +63,7 @@ sax_dat$Date=as.POSIXct(sax_dat$Date, tz="GMT", format = "%Y-%m-%d")
 sax_dat_raw=sax_dat%>%
   mutate(year=year(Date), month=month(Date), dia=day(Date), species="Saxifraga")%>%
   mutate(DOY=yday(Date))%>%
-  select(-General_remarks, -Date)%>%
+  select(-General_remarks)%>%
   filter(Plot%in%c("Sax1", "Sax2", "Sax3"))
 
 ###silene####
@@ -75,7 +75,7 @@ sil_dat$Date=as.POSIXct(sil_dat$Date, tz="GMT", format = "%Y-%m-%d")
 sil_dat_raw=sil_dat%>%
   mutate(year=year(Date), month=month(Date), dia=day(Date), species="Silene")%>%
   mutate(DOY=yday(Date))%>%
-  select(-General_Remarks, -Field_Remarks, -Date)%>%
+  select(-General_Remarks, -Field_Remarks)%>%
   filter(Plot%in%c("Sil1", "Sil2", "Sil3", "Sil4"))
 
 #all plants####
@@ -89,16 +89,57 @@ plant_dat_clean=plant_dat_raw%>%
   mutate(tot_flower=rowSums(across(c(Male_flowers, Female_flowers, Flowers, Open)), na.rm=T),
          tot_bud=Buds,
          tot_senescent=rowSums(across(c(Senescent, Seed_hairs)), na.rm=T))%>%
-  select(Plot, Section, year, month, dia, species, DOY, tot_flower, tot_bud, tot_senescent)%>%
-  filter(!year<1997, month%in%c(6:9))
+  select(Plot, Section, year, Date, month, dia, species, DOY, tot_flower, tot_bud, tot_senescent)%>%
+  filter(!year<1996, month%in%c(6:9))
 
 #data reshaping####
 plant_dat=plant_dat_clean%>%
-  group_by(Plot,species, year, DOY)%>%summarise(tot_buds=sum(tot_bud),
+  group_by(Plot,species, year, DOY, Date)%>%summarise(tot_buds=sum(tot_bud),
                                                 tot_flowers=sum(tot_flower),
-                                                tot_sen=sum(tot_senescent))
+                                                tot_sen=sum(tot_senescent))%>%
+  filter()
 
-plant_datA <- plant_dat %>%
+#data checks###
+
+#include only plots and years that pass Hartigan's dip test for unimodality
+library(diptest)
+
+dip_results <- plant_dat %>%
+  group_by(species, Plot, year) %>%
+  summarise(
+    D_statistic_buds = if (length(unique(tot_buds)) > 3) {
+      tryCatch(dip.test(tot_buds)$statistic, error = function(e) NA)
+    } else NA,
+    p_value_buds = tryCatch(dip.test(tot_buds)$p.value, error = function(e) NA),
+    D_statistic_flowers = if (length(unique(tot_flowers)) > 3) {
+      tryCatch(dip.test(tot_flowers)$statistic, error = function(e) NA)
+    } else NA,
+    p_value_flowers = tryCatch(dip.test(tot_flowers)$p.value, error = function(e) NA),
+    D_statistic_senescent = if (length(unique(tot_sen)) > 3) {
+      tryCatch(dip.test(tot_sen)$statistic, error = function(e) NA)
+    } else NA,
+    p_value_senescent = tryCatch(dip.test(tot_sen)$p.value, error = function(e) NA),
+    n = n(), # p <0.05 multimodality, p>0.05 unimodal
+    .groups = "drop"
+  ) %>%
+  mutate(
+    unimodal_buds = ifelse(is.na(p_value_buds), NA, p_value_buds > 0.05),
+    unimodal_flowers = ifelse(is.na(p_value_flowers), NA, p_value_flowers > 0.05),
+    unimodal_senescent = ifelse(is.na(p_value_senescent), NA, p_value_senescent > 0.05),
+    Include= ifelse(unimodal_buds&unimodal_flowers&unimodal_senescent==TRUE, 1, 0)
+  )
+
+include_dat=dip_results%>%filter(Include==1)%>%
+  select(species, Plot, year, Include)
+
+yr_ct <- include_dat %>%
+  group_by(species, Plot) %>%
+  summarise(nyr = n_distinct(year), .groups = "drop")
+
+plant_df=plant_dat%>%
+  semi_join(include_dat)
+
+plant_datA <- plant_df %>%
   mutate(
     tot_F = tot_flowers,
     tot_NF = tot_buds + tot_sen
