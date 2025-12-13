87df465e79fda751e6de3a04a608e9d69267e6b6
Patricia <patdumandan@gmail.com>
2025-12-13 21:54:11 +0100

    add script for plant_phen7 diagnostics

--- model_diagnostics.R
+++ model_diagnostics.R
@@ -2,11 +2,9 @@
 library(bayesplot)
 library(posterior)
 
-DOY <- seq(-2, 2, length = 100)
-
 #Dryas####
 
-drymod_draws <- dry_mod4b$draws()%>%posterior::as_draws_array()
+drymod6b1_draws <- dry_mod6b1$draws()%>%posterior::as_draws_array()
 
 varnames <- variables(drymod_draws)
 mu_params <- grep("^mu\\[", varnames, value = TRUE)
@@ -16,29 +14,27 @@ mcmc_trace(drymod_draws, pars = mu_params)
 mcmc_trace(drymod_draws, pars = width_params)
 mcmc_trace(drymod_draws, pars = c("sigma_mu","sigma_width", "sigma_year"))
 
-mu_draws= dry_mod4b$draws("mu_year")
+mu_draws= dry_mod6b1$draws("mu_year")
 mcmc_areas(mu_draws, regex = "mu_year")
 
-width_draws= dry_mod4b$draws("width")
+width_draws= dry_mod6b1$draws("width")
 mcmc_areas(width_draws, regex = "width")
 
 # Rhat values
-rhat_val=dry_mod4b$summary()$rhat
-rhat_vals <- posterior::rhat(drymod_draws)
-ess_vals <- posterior::ess_bulk(drymod_draws)   # numeric vector
-n_draws  <- posterior::ndraws(drymod_draws)              # total draws per chain × chains
+ess_vals <- posterior::ess_bulk(drymod6b1_draws)
+n_draws  <- posterior::ndraws(drymod6b1_draws)
 
 neff_ratio_vals <- ess_vals / n_draws
 
-p1 <- mcmc_rhat_hist(rhat_val, bins=10)
-p2 <- mcmc_rhat(rhat_val)
-p3 <- mcmc_neff_hist(neff_ratio_vals)
+p1= mcmc_rhat_hist(dry_mod6b1$summary()$rhat)
+p2= mcmc_rhat(dry_mod6b1$summary()$rhat)
+p3= mcmc_neff_hist(neff_ratio_vals, bins=10)
 
-ggarrange(p1, p2, p3, nrow = 3)
+ggarrange(p1, p2, p3, ncol = 3)
 
 # Regular, visual, posterior predictive checks
-color_scheme_set("red")
-yrep = dry_mod4b$draws(format="draws_matrix", variables="y_pred")
+color_scheme_set("blue")
+yrep = dry_mod6b1$draws(format="draws_matrix", variables="y_pred")
 yrep <- yrep[(nrow(yrep)-99):nrow(yrep), ]  # replicate data, take only the last 100 samples to speed up drawing
 ggarrange(ppc_dens_overlay(y = dry_datA$tot_F,
                            yrep = yrep),
@@ -49,12 +45,13 @@ ggarrange(ppc_dens_overlay(y = dry_datA$tot_F,
           nrow = 2, ncol=2)
 ppc_hist(y = dry_datA$tot_F, yrep[1:11,])
 
-mu         <- dry_mod4b$draws("mu_year", format="draws_matrix")
-width      <- dry_mod4b$draws("width", format="draws_matrix")
-alpha_year <- dry_mod4b$draws("alpha_year", format="draws_matrix")
-u_plot     <- dry_mod4b$draws("u_plot", format="draws_matrix")
-u_plot_mu     <- dry_mod4b$draws("u_plot_mu", format="draws_matrix")
+mu         <- dry_mod6b1$draws("mu", format="draws_matrix")
+width      <- dry_mod6b1$draws("width", format="draws_matrix")
+alpha_year <- dry_mod6b1$draws("alpha_year", format="draws_matrix")
+u_plot     <- dry_mod6b1$draws("u_plot", format="draws_matrix")
+u_plot_mu  <- dry_mod6b1$draws("u_plot_mu", format="draws_matrix")
 
+#per year####
 DOY = seq(-2, 2, length = 100)
 invLogit <- function(x) exp(x) / (1 + exp(x))
 
@@ -75,7 +72,7 @@ for (y in 1:ncol(mu)) {            # years
       # compute eta for each draw, then average
 
       eta_post= alpha_year[, y]+u_plot[, p_idx] -
-          (DOY[nd]-(mu[, y] + u_plot_mu[, p_idx]))^2/(2 * width[, y]^2)
+          (DOY[nd]-(mu[, y] + u_plot_mu[, p_idx]))^2/(width[, y]^2)
 
       eta_hat[nd] <- mean(invLogit(eta_post))
     }
@@ -93,3 +90,221 @@ for (y in 1:ncol(mu)) {            # years
            col=cols[pl])
   }
 }
+
+#per plot####
+DOY <- seq(-2, 2, length = 100)
+invLogit <- function(x) exp(x) / (1 + exp(x))
+
+year.ids <- sort(unique(dryas_data$year_id))
+YR <- length(year.ids)
+cols <- rainbow(YR)
+
+plot.ids <- sort(unique(dryas_data$plot_id))
+P <- length(plot.ids)
+
+par(mfrow = c(3, 2))
+
+for (pl in plot.ids) {                 # LOOP OVER PLOTS
+
+  plot(NULL, xlim = range(DOY), ylim = c(0, 1),
+       xlab = "DOY", ylab = "Proportion F",
+       main = paste("Plot", pl))
+
+  for (y in 1:YR) {                    # LOOP OVER YEARS
+
+    yr_idx <- year.ids[y]
+    eta_hat <- numeric(length(DOY))
+
+    for (nd in seq_along(DOY)) {
+
+      eta_post <- alpha_year[, y] +
+        u_plot[, pl] -
+        (DOY[nd] - (mu[, y] + u_plot_mu[, pl]))^2 /
+        (width[, y]^2)
+
+      eta_hat[nd] <- mean(invLogit(eta_post))
+    }
+
+    # posterior mean line for this year
+    lines(DOY, eta_hat, col = cols[y], lwd = 2)
+
+    # observed data
+    ind <- dryas_data$plot_id == pl &
+      dryas_data$year_id == yr_idx
+
+    points(dryas_data$DOYs[ind],
+           dryas_data$tot_F[ind] /
+             (dryas_data$tot_F[ind] + dryas_data$tot_NF[ind]),
+           col = cols[y], pch = 16)
+  }
+}
+
+#diagnostics for plant_phen7
+drymod7_draws <- dry_mod7$draws()%>%posterior::as_draws_array()
+ess_vals <- posterior::ess_bulk(drymod7_draws)   # numeric vector
+n_draws  <- posterior::ndraws(drymod7_draws)              # total draws per chain × chains
+
+neff_ratio_vals <- ess_vals / n_draws
+
+p1= mcmc_rhat_hist(dry_mod7$summary()$rhat)
+p2= mcmc_rhat(dry_mod7$summary()$rhat)
+p3= mcmc_neff_hist(neff_ratio_vals, bins=10)
+
+ggarrange(p1, p2, p3, ncol = 3)
+
+color_scheme_set("blue")
+yrep = dry_mod7$draws(format="draws_matrix", variables="y_pred")
+yrep <- yrep[(nrow(yrep)-99):nrow(yrep), ]  # replicate data, take only the last 100 samples to speed up drawing
+ggarrange(ppc_dens_overlay(y = dry_datA$tot_F,
+                           yrep = yrep),
+          ppc_ecdf_overlay(y = dry_datA$tot_F,
+                           yrep = yrep),
+          ppc_pit_ecdf(y = dry_datA$tot_F, yrep = yrep, prob = 0.99, plot_diff = FALSE),
+          ppc_pit_ecdf(y = dry_datA$tot_F, yrep = yrep, prob = 0.99, plot_diff = TRUE),
+          nrow = 2, ncol=2)
+mu              <- dry_mod7$draws("mu", format = "draws_matrix")
+alpha_year      <- dry_mod7$draws("alpha_year", format = "draws_matrix")
+u_plot          <- dry_mod7$draws("u_plot", format = "draws_matrix")
+u_plot_mu       <- dry_mod7$draws("u_plot_mu", format = "draws_matrix")
+
+width_L_year    <- dry_mod7$draws("width_L_year", format = "draws_matrix")
+width_R_year    <- dry_mod7$draws("width_R_year", format = "draws_matrix")
+
+width_L_plot    <- dry_mod7$draws("width_L_plot", format = "draws_matrix")
+width_R_plot    <- dry_mod7$draws("width_R_plot", format = "draws_matrix")
+
+DOY <- seq(-2, 2, length = 150)
+invLogit <- plogis
+
+plot.ids <- sort(unique(dryas_data$plot_id))
+P <- length(plot.ids)
+
+cols <- rainbow(P)
+
+par(mfrow = c(3, 4))
+for (y in 1:ncol(mu)) {          # loop over years
+  for (pl in 1:P) {              # loop over plots
+
+    p_idx <- plot.ids[pl]
+    eta_hat <- numeric(length(DOY))
+
+    for (nd in seq_along(DOY)) {
+
+      # distance from peak (per posterior draw)
+      d <- DOY[nd] - (mu[, y] + u_plot_mu[, p_idx])
+
+      # year × plot widths
+      width_L_np <- width_L_year[, y] * width_L_plot[, p_idx]
+      width_R_np <- width_R_year[, y] * width_R_plot[, p_idx]
+
+      penalty <- ifelse(
+        d < 0,
+        d^2 / (2 * width_L_np^2),
+        d^2 / (2 * width_R_np^2)
+      )
+
+      eta_post <-
+        alpha_year[, y] +
+        u_plot[, p_idx] -
+        penalty
+
+      eta_hat[nd] <- mean(invLogit(eta_post))
+    }
+
+    # draw curves
+    if (pl == 1) {
+      plot(
+        DOY, eta_hat, type = "l",
+        ylim = c(0, 1),
+        col = cols[pl],
+        main = paste("Year", y),
+        xlab = "DOY", ylab = "Probability"
+      )
+    } else {
+      lines(DOY, eta_hat, col = cols[pl])
+    }
+
+    # observed data
+    ind <- dryas_data$plot_id == p_idx &
+      dryas_data$year_id == y
+
+    points(
+      dryas_data$DOYs[ind],
+      dryas_data$tot_F[ind] /
+        (dryas_data$tot_F[ind] + dryas_data$tot_NF[ind]),
+      col = cols[pl],
+      pch = 16
+    )
+  }
+}
+
+#per plot#####
+DOY <- seq(-2, 2, length = 150)
+invLogit <- plogis
+
+year.ids <- sort(unique(dryas_data$year_id))
+YR <- length(year.ids)
+
+plot.ids <- sort(unique(dryas_data$plot_id))
+P <- length(plot.ids)
+
+cols <- rainbow(YR)
+
+par(mfrow = c(3, 2))
+
+for (pl in plot.ids) {   # LOOP OVER PLOTS
+
+  plot(
+    NULL,
+    xlim = range(DOY),
+    ylim = c(0, 1),
+    xlab = "DOY",
+    ylab = "Proportion F",
+    main = paste("Plot", pl)
+  )
+
+  for (y in seq_along(year.ids)) {  # LOOP OVER YEARS
+
+    yr_idx <- year.ids[y]
+    eta_hat <- numeric(length(DOY))
+
+    for (nd in seq_along(DOY)) {
+
+      # distance from peak (vector over posterior draws)
+      d <- DOY[nd] - (mu[, y] + u_plot_mu[, pl])
+
+      # year × plot widths
+      width_L_np <- width_L_year[, y] * width_L_plot[, pl]
+      width_R_np <- width_R_year[, y] * width_R_plot[, pl]
+
+      penalty <- ifelse(
+        d < 0,
+        d^2 / (2 * width_L_np^2),
+        d^2 / (2 * width_R_np^2)
+      )
+
+      eta_post <-
+        alpha_year[, y] +
+        u_plot[, pl] -
+        penalty
+
+      eta_hat[nd] <- mean(invLogit(eta_post))
+    }
+
+    # posterior mean curve
+    lines(DOY, eta_hat, col = cols[y], lwd = 2)
+
+    # observed data
+    ind <- dryas_data$plot_id == pl &
+      dryas_data$year_id == yr_idx
+
+    points(
+      dryas_data$DOYs[ind],
+      dryas_data$tot_F[ind] /
+        (dryas_data$tot_F[ind] + dryas_data$tot_NF[ind]),
+      col = cols[y],
+      pch = 16
+    )
+  }
+}
+
